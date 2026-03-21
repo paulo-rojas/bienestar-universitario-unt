15153816bb4b343c19bf89a8c1d9e864e60ab184
Paulo Rojas <paulo1rojas2@gmail.com>
2026-03-21 18:47:37 -0500

    initial commit

---
+++ src/test/java/pe/edu/unitru/bienestar/BienestarApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unitru.bienestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
